c3286e136835e19919848b9428ccca1d2aa4afdc
StanimalTheMan <sjc591@nyu.edu>
2022-12-16 13:28:06 -0500

    Define CRUD functionalities

--- src/verse/verse.service.ts
+++ src/verse/verse.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateVerseDto } from './dto/create-verse.dto';
+import { UpdateVerseDto } from './dto/update-verse.dto';
 import { Verse, VerseDocument } from './schemas/verse.schema';
 
 @Injectable()
@@ -8,4 +10,26 @@ export class VerseService {
   constructor(
     @InjectModel(Verse.name) private readonly model: Model<VerseDocument>,
   ) {}
+
+  async findAll(): Promise<Verse[]> {
+    return await this.model.find().exec();
+  }
+
+  async findOne(id: string): Promise<Verse> {
+    return await this.model.findById(id).exec();
+  }
+
+  async create(createVerseDto: CreateVerseDto): Promise<Verse> {
+    return await new this.model({
+      ...createVerseDto,
+    }).save();
+  }
+
+  async update(id: string, updateVerseDto: UpdateVerseDto): Promise<Verse> {
+    return await this.model.findByIdAndUpdate(id, updateVerseDto).exec();
+  }
+
+  async delete(id: string): Promise<Verse> {
+    return await this.model.findByIdAndDelete(id).exec();
+  }
 }
