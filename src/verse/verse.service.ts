c65deb8181df43635c0e72931bbfb7544571c44e
StanimalTheMan <sjc591@nyu.edu>
2022-12-16 12:59:42 -0500

    Inject Model in VerseService

--- src/verse/verse.service.ts
+++ src/verse/verse.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Verse, VerseDocument } from './schemas/verse.schema';
 
 @Injectable()
-export class VerseService {}
+export class VerseService {
+  constructor(
+    @InjectModel(Verse.name) private readonly model: Model<VerseDocument>,
+  ) {}
+}
